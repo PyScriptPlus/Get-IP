352ce216afcc2b62b8199bc0c85f120f2f7d8414
Mohammad <91163661+Sardar126@users.noreply.github.com>
2024-05-27 16:21:53 +0330

    Add files via upload

---
+++ IP.js
@@ -0,0 +1,25 @@
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $.get("https://api.ipify.org", function(data, status){
+//         alert("Data: " + data + "\nStatus: " + status);
+//       });
+//     });
+//   });
+
+
+// $(document).ready(function(){
+//     $("button").click(function(){
+//       $.get("https://api.ipify.org",
+//       {
+//         name: "Donald Duck",
+//         city: "Duckburg"
+//       },
+//       function(data,status){
+//         alert("Data: " + data + "\nStatus: " + status);
+//       });
+//     });
+//   });
+
+
+
+$("#hello").load("Hello my brother #ip")
